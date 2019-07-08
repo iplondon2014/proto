8a58cd55b5aea9b1ad5001d2d43f80e228bd91ef
Ishwar Pandeya <ip.london2014@gmail.com>
2019-07-08 22:36:46 +0100

    generate-image endpoint with dummy implementation added

--- src/main/java/org/fcg/proto/ProtoController.java
+++ src/main/java/org/fcg/proto/ProtoController.java
@@ -2,6 +2,7 @@ package org.fcg.proto;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,10 @@ public class ProtoController {
     @RequestMapping(value = "/ping", produces = "text/plain")
     public String ping(){
         return "ok";
+    }
+
+    @RequestMapping(value = "/generate-image", consumes = "application/json", method = RequestMethod.POST)
+    public String generateImage(){
+        return "";
     }
 }
